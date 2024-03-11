2927512fba3cdfc792b6e68098c24f123241011c
andres gomez <161092979+andrego151k@users.noreply.github.com>
2024-03-11 16:24:43 -0500

    Update 02c2-3_tarifasminuto.py
    cambio del el nombre op_costo y comentarios

--- e-conocimiento m12/02c2-3_tarifasminuto.py
+++ e-conocimiento m12/02c2-3_tarifasminuto.py
@@ -1,43 +1,32 @@
-#  punto 2 tarifas para cada región
-tarifa_eeuu = 900
+tarifa_eeuu = 900   # punto 2 tarifas para cada región
 tarifa_canada = 800
 tarifa_europa = 950
 tarifa_resto_mundo = 1250
-
-#  duración para aplicar 20% menos
-umbral_descuento = 15
+umbral_descuento = 15   # duración para aplicar 20% menos
 
 
-def operacion_costo(tarifa, duracion):
-    costo_total = tarifa * duracion
-
-    # Aplicar 20% desc si dura mayor al umbral
+def op_costo(cobromin, duracion):
+    costo_total = cobromin * duracion
     if duracion >= umbral_descuento:
-        costo_total *= 0.8
-
+        costo_total *= 0.8  # Aplicar 20% desc si dura mayor al umbral
     return costo_total
 
 
-while True:
-    # menú regiones
-    print("\nSeleccione la región de la llamada:")
+while True:   # menú regiones
+    print("punto 2c \nSeleccione la región de la llamada:")
     print("1. Estados Unidos")
     print("2. Canadá")
     print("3. Europa")
     print("4. Resto del mundo")
     print("5. Salir")
-
     opcion = input("pon el número del menu región (1-5): ")
 
     if opcion == '5':
         print("Saliendo del programa.")
         break
-    elif opcion in ['1', '2', '3', '4']:
-        # input duracion para determinar si desc
+    elif opcion in ['1', '2', '3', '4']:  # input duracion para determinar si desc
         duracion = int(input("pon la duración, 20% off si es mas de 15 : "))
-
-        # condional de cada region
-        if opcion == '1':
+        if opcion == '1':   # condional de cada region
             tarifa = tarifa_eeuu
         elif opcion == '2':
             tarifa = tarifa_canada
@@ -45,12 +34,7 @@ while True:
             tarifa = tarifa_europa
         else:
             tarifa = tarifa_resto_mundo
-
-        # costo total punto 3
-        costo_total = operacion_costo(tarifa, duracion)
-
-        # punto 3 costo total
-
+        costo_total = op_costo(tarifa, duracion)   # costo total punto 3
         print("\n El costo total de la llamada es: $", costo_total)
         print("             \t tarifa original a: $", tarifa)
     else:
